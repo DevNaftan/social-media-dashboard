930e57c0202e83775b0895951da799190437ccf6
Naftan <x.X.Naftan.X.x@gmail.com>
2020-08-23 17:50:22 -0500

    Set dark mode default

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@ const $checkbox = document.getElementById('checkbox')
 
 function mode(event) {
   if (event.target.checked) {
+    document.body.classList.remove('dark')
+    document.body.classList.add('light')
+  } else {
     document.body.classList.remove('light')
     document.body.classList.add('dark')
-  } else {
-    document.body.classList.add('light')
-    document.body.classList.remove('dark')
   }
 }
 
